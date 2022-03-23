959556af78f28a8f7509d9d018dfdc12db29a573
bitsbb01 <61090794+bitsbb01@users.noreply.github.com>
2022-03-22 22:15:28 -0500

    Update main.go

--- main.go
+++ main.go
@@ -182,18 +182,7 @@ func main() {
 	urlList := [...]string{
 
 		
-		"https://iptvcat.com/s/mbc/mark/mbc",
-		"https://iptvcat.com/s/sky/mark/sky",
-		"https://iptvcat.com/s/movie/mark/movie",
-		"https://iptvcat.com/s/star/mark/star",
-		"https://iptvcat.com/s/hbo/mark/hbo",
-		"https://iptvcat.com/s/hallmark/mark/hallmark",
-		"https://iptvcat.com/s/lifetime/mark/lifetime",
-		"https://iptvcat.com/s/cinema/mark/cinema",
-		"https://iptvcat.com/s/lifetime/mark/lifetime",
-		"https://iptvcat.com/s/disney/mark/disney",
-		"https://iptvcat.com/s/netflix/mark/netflix",
-		"https://iptvcat.com/turkey",
+		"https://iptvcat.com/canada",
 		}
 
 	for _, element := range urlList {
